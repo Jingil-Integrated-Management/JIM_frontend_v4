b83a3a904bfc7eaa329e169536cf958bce0147cd
James Jung <therealjamesjung@gmail.com>
2022-01-03 17:17:37 +0900

    refactor: part interface

---
+++ src/types/Part.d.ts
@@ -0,0 +1,23 @@
+export interface Part {
+  id?: number;
+  x: string;
+  y: string;
+  z: string;
+  quantity: number;
+  price: string | null;
+  comment?: string | null;
+  drawing: number;
+  created_at?: string;
+  division: number;
+  material: string;
+  outsource?: number | null;
+  file?: number | null;
+  client__name?: string;
+  client__id?: number;
+  file_name?: string | null;
+  division__main_division?: string;
+  division__dub_division?: string;
+  drawing__name?: string;
+  outsource_info?: [] | null;
+  fileSrc?: string;
+}
