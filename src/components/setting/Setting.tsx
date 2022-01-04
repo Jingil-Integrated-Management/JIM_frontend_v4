f26106d447a91034105c6767eda45266225acc99
byhhh2 <rlaalsdl111@gmail.com>
2022-01-04 21:49:32 +0900

    refactor: change setting structure

--- src/components/setting/Setting.tsx
+++ src/components/setting/Setting.tsx
@@ -1,14 +1,8 @@
-import { useState, useEffect } from 'react';
-
 //drawer
 import Drawer from '@mui/material/Drawer';
 
 //icons
 import CloseIcon from '@mui/icons-material/Close';
-
-//axios
-import webClient from '../../utils/Webclient';
-import { AxiosResponse } from 'axios';
 
 //types
 import { ClientData } from '../../types';
@@ -18,32 +12,21 @@ import ClientSetting from './ClientSetting';
 import NavigationSetting from './NavigationSetting';
 import DivisionSetting from './DivisionSetting';
 
+//redux
+import { connect } from 'react-redux';
+
 interface SettingProps {
   open: boolean;
   setOpen: Function;
+  clientList: ClientData[];
 }
 
-const Setting = ({ open, setOpen }: SettingProps) => {
-  const [clientList, setClientList] = useState<ClientData[]>([]);
-
-  useEffect(() => {
-    const getClientList = async () => {
-      try {
-        const response: AxiosResponse = await webClient.get(`client/`);
-        setClientList(response.data as ClientData[]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getClientList();
-  }, []);
-
+const Setting = ({ open, setOpen, clientList }: SettingProps) => {
   return (
     <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
       <div className="w-608 px-32 py-40">
         <div className="flex items-center justify-between">
-          <span className="title-text">설정하기</span>
+          <span className="setting-title-text">설정하기</span>
           <CloseIcon
             fontSize="small"
             className="cursor-pointer"
@@ -58,4 +41,8 @@ const Setting = ({ open, setOpen }: SettingProps) => {
   );
 };
 
-export default Setting;
+const mapStateToProps = (state: any) => ({
+  clientList: state.clientReducer.clientList,
+});
+
+export default connect(mapStateToProps)(Setting);
