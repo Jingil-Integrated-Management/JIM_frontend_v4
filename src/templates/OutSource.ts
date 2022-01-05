6ef7e47bf271b426c239005ddf3e3c12d08c3c6d
James <jamesj0107@naver.com>
2022-01-05 16:06:48 +0900

    feat: add outsource template

---
+++ src/templates/OutSource.ts
@@ -0,0 +1,14 @@
+import { OutsourceData } from '../types';
+
+const Outsource = (): OutsourceData => ({
+  material_price: null,
+  milling_price: null,
+  heat_treat_price: null,
+  wire_price: null,
+  material_client: null,
+  milling_client: null,
+  heat_treat_client: null,
+  wire_client: null,
+});
+
+export default Outsource;
