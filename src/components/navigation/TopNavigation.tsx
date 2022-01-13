0dd1df32b873309f95a692a677f38fc59290ddba
JINHYUNG IAN LEE <luciancah@gmail.com>
2022-01-14 00:37:58 +0900

    fix: adjusted margin on buttons / nav bar

--- src/components/navigation/TopNavigation.tsx
+++ src/components/navigation/TopNavigation.tsx
@@ -59,7 +59,7 @@ const TopNavigation = (props: topNaviProps) => {
         <div className="flex items-center">
           <Search clientList={clientList} />
           <button
-            className="w-40 h-40 ml-4 flex justify-center items-center rounded-8 icon-button cursor-pointer relative"
+            className="w-40 h-40 flex justify-center items-center rounded-8 icon-button cursor-pointer relative"
             onClick={() => props.setDrawerOpen(true)}
           >
             <AddIcon />
@@ -67,7 +67,7 @@ const TopNavigation = (props: topNaviProps) => {
               <div className="bg-font-accent w-5 h-5 absolute top-5 right-5 rounded-20"></div>
             )}
           </button>
-          <div className="w-40 h-40 ml-4 flex justify-center items-center rounded-8">
+          <div className="w-40 h-40 flex justify-center items-center rounded-8">
             <img src={logo} alt="AD"></img>
           </div>
         </div>
