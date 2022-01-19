7a5891a9c4c65483531deca2b9b67d58bf468bdc
byhhh2 <rlaalsdl111@gmail.com>
2022-01-19 16:55:18 +0900

    fix: delete using client list before load

--- src/utils/getClientID.ts
+++ src/utils/getClientID.ts
@@ -1,7 +1,7 @@
 import { ClientData } from '../types';
 
 const getClientID = (clientList: ClientData[], clientName: string) => {
-  for (let i = 0; i < clientList.length; i++) {
+  for (let i = 0; i < clientList?.length; i++) {
     if (clientName === clientList[i].name) return clientList[i].id as number;
   }
   return -1;
